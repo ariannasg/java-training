33f8438474f87664627494cc6d9cf066ede4c576
Ari <ariannasg.ldn@gmail.com>
2020-05-22 12:36:48 +0100

    Added class for trying the while loop

---
+++ src/Loops.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        boolean isOnRepeat = true;
+        Scanner input = new Scanner(System.in);
+
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+
+            if (input.next().equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
+
+        System.out.println("Playing next song");
+    }
+}
+
+//CONSOLE OUTPUT:
+//Playing current song
+//Would you like to take this song off of repeat? If so, answer yes
+//no
+//Playing current song
+//Would you like to take this song off of repeat? If so, answer yes
+//no
+//Playing current song
+//Would you like to take this song off of repeat? If so, answer yes
+//yes
+//Playing next song
