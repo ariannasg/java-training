36354eb02a3e368b7243950309cd3e08f4435cc0
Ari <ariannasg.ldn@gmail.com>
2020-05-22 11:45:48 +0100

    Added a class for trying String operations

---
+++ src/Strings.java
@@ -0,0 +1,26 @@
+public class Strings {
+    public static void main(String[] args) {
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
+
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
+        System.out.println("String operators");
+        char studentFirstInitial = studentFirstName.charAt(0);
+        System.out.println(studentFirstInitial);
+        char studentLastInitial = studentLastName.charAt(0);
+        System.out.println(studentLastInitial);
+
+        double studentGPA = 3.45;
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of: " + studentGPA);
+    }
+}
+
+//CONSOLE OUTPUT:
+//Kayla
+//Hammon
+//String operators
+//K
+//H
+//Kayla Hammon has a GPA of: 3.45
