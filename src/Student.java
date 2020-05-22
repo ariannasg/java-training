9716cd76e876b523130d12761c15ece6fff63b86
Ari <ariannasg.ldn@gmail.com>
2020-05-22 11:28:52 +0100

    Added a Student class for trying primitive types

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'a';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
+
+//CONSOLE OUTPUT:
+//15
+//3.45
+//A
+//a
+//true
