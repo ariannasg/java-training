2eb4360d1155f9a70cf1733babf20817cd395bb6
Ari <ariannasg.ldn@gmail.com>
2020-05-22 15:44:11 +0100

    Created a Triangle class for trying instance vars and methos, as well as class vars and methods

---
+++ src/Triangle.java
@@ -0,0 +1,50 @@
+public class Triangle {
+    // class variables (or static vars)
+    static int numOfSides = 3;
+
+    // instance variables (or non-static vars)
+    double base;
+    double height;
+    double sideLenOne;
+    double sideLenTwo;
+    double sideLenThree;
+
+    public Triangle(double base, double height, double sideLenOne, double sideLenTwo, double sideLenThree) {
+        this.base = base;
+        this.height = height;
+        this.sideLenOne = sideLenOne;
+        this.sideLenTwo = sideLenTwo;
+        this.sideLenThree = sideLenThree;
+    }
+
+    // this is an instance method or non-static method. It needs to be called using an instance of the class.
+    public double findArea() {
+        return (this.base * this.height) / 2;
+    }
+
+    // this is a class method or static method. It needs to be called using the class name directly.
+    public static void showPurpose() {
+        System.out.println("This is the representation of a Triangle");
+    }
+
+    public static void main(String[] args) {
+        Triangle triangleA = new Triangle(10, 10, 10, 10, 10);
+        Triangle triangleB = new Triangle(8, 15, 5, 5, 5);
+
+        System.out.println("TriangleA area is " + triangleA.findArea());
+        System.out.println("TriangleA base is " + triangleA.base);
+        System.out.println("TriangleB area is " + triangleB.findArea());
+        System.out.println("TriangleB height is " + triangleB.height);
+
+        Triangle.showPurpose();
+        System.out.println("A Triangle has " + Triangle.numOfSides + " sides.");
+    }
+}
+
+//CONSOLE OUTPUT:
+//TriangleA area is 50.0
+//TriangleA base is 10.0
+//TriangleB area is 60.0
+//TriangleB height is 15.0
+//This is the representation of a Triangle
+//A Triangle has 3 sides.
