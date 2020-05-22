d8726c5b880940887b55c4671751812f060e85c9
Ari <ariannasg.ldn@gmail.com>
2020-05-22 17:47:17 +0100

    Created a class for trying out running a java file passing arguments from the console

---
+++ src/essentials/ConsoleArgs.java
@@ -0,0 +1,10 @@
+package essentials;
+
+public class ConsoleArgs {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+    }
+}
+
+//When running java essentials.ConsoleArgs Ari -> the output is: Hello Ari
+
