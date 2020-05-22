e1e24011bcd2889383be6c5637f98b0bd731b32d
Ari <ariannasg.ldn@gmail.com>
2020-05-22 13:48:48 +0100

    Added a MultipleChoiceQuestion class for practicing loops, flow control and user inputs

---
+++ src/MultipleChoiceQuestion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultipleChoiceQuestion {
+    public static void main(String[] args) {
+        String madrid = "Madrid";
+        String barcelona = "Barcelona";
+        String valencia = "Valencia";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the capital of Spain? " + madrid + ", " + barcelona + " or " + valencia);
+        System.out.println("Type in the correct choice");
+
+        while (true) {
+            if (input.next().equalsIgnoreCase(madrid)) {
+                System.out.println("That's the correct answer");
+                break;
+            } else {
+                System.out.println("That answer is incorrect. Try again");
+            }
+        }
+    }
+}
