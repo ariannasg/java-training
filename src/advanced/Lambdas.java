c55cbf0139d1c5c7adea82d328ba54c21b31b8e5
Ari <ariannasg.ldn@gmail.com>
2020-05-23 18:33:00 +0100

    Updated comment in the Lambdas class

--- src/advanced/Lambdas.java
+++ src/advanced/Lambdas.java
@@ -1,6 +1,6 @@
 package advanced;
 
-// from java 8 we can use lambdas.
+// since java 8 we can use lambdas.
 // lambdas provide a quick and simple way of implementing functional interfaces.
 public class Lambdas {
     public static void main(String[] args) {
