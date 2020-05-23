eafbac52c9408b36b5b0312755f254ef4918af5f
Ari <ariannasg.ldn@gmail.com>
2020-05-23 10:26:39 +0100

    Added class for trying string interpolation

---
+++ src/essentials/StringInterpolation.java
@@ -0,0 +1,24 @@
+package essentials;
+
+// String interpolation is the process of evaluating a string literal containing one or more placeholders
+public class StringInterpolation {
+    public static void main(String[] args) {
+        var item = "Shirt";
+        var size = "M";
+        var price = 14.99;
+        var color = "red";
+
+        var template = "Clothing item: %s, size: %s, color: %s, price: $%f";
+        System.out.println(template.formatted(item, size, color, price));
+
+        var stringFormat = String.format(template, item, size, color, price);
+        System.out.println(stringFormat);
+
+        template = "Clothing item: %s, size: %s, color: %s, price: $%.2f";
+        System.out.println(template.formatted(item, size, color, price));
+
+    }
+}
+
+//CONSOLE OUTPUT:
+
